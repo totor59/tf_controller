b8195505e8934eeb9b72ce6ae8f2d3032678ed62
Victor MARECHAL <victor.marechal@valiuz.com>
2024-02-02 16:43:48 +0100

    test tf-controller

--- clusters/poc-cluster/storage.tf
+++ clusters/poc-cluster/storage.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "default" {
-  name          = "bucket-test"
+  name          = "bucket-test1"
   force_destroy = false
   location      = "europe-west1-b"
   storage_class = "STANDARD"
